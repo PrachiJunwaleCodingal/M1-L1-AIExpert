df878dac950c132b6d8df53e7f3571cadd10b67f
PrachiJunwaleCodingal <prachi.junwale@gmail.com>
2025-06-30 14:05:35 +0530

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,14 @@
+
+print("Hello!I am AIBot.What's your name?")
+name=input()
+print(f'Nice to meet you, {name}!')	
+	
+print('How  are you feeling today?(good/bad):')
+mood=input().lower()	
+if mood	=="good" :	
+    print('Great!!')	
+elif mood=='bad':	
+    print("I am so sorry to hear that")
+else:	
+    print('I see..')
+    print('It was nice chatting with you')	
